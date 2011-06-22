6d21c95ae455c4e9e4ed9f9182e0da6805be8b3b
Adam Goucher <adam@goucher.ca>
2011-06-22 15:57:06 -0400

    more tests. oh. and they pass

--- testing/test_collection.py
+++ testing/test_collection.py
@@ -72,3 +72,71 @@ class Test_collection:
         passed, skipped, failed = reprec.listoutcomes()
         assert len(passed) == 1
         assert passed[0].nodeid.split('::')[1] == 'test_baz'
+        
+    def test_check_collect_foo_or_bar(self, testdir):
+        p = testdir.makepyfile("""
+            import pytest
+
+            @pytest.mark.bar
+            def test_foo():
+                pass
+
+            @pytest.mark.foo
+            def test_bar():
+                pass
+
+            @pytest.mark.foo
+            @pytest.mark.bar
+            def test_baz():
+                pass
+        """)
+        reprec = testdir.inline_run("-s", '-f', "foo", "-f", "bar", p)
+        passed, skipped, failed = reprec.listoutcomes()
+        assert len(passed) == 3
+        acceptable = ['test_baz', 'test_foo', 'test_bar']
+        for passd in passed:
+            assert passd.nodeid.split('::')[1] in acceptable
+
+    def test_check_collect_foo_but_not_bar(self, testdir):
+        p = testdir.makepyfile("""
+            import pytest
+
+            @pytest.mark.bar
+            def test_foo():
+                pass
+
+            @pytest.mark.foo
+            def test_bar():
+                pass
+
+            @pytest.mark.foo
+            @pytest.mark.bar
+            def test_baz():
+                pass
+        """)
+        reprec = testdir.inline_run("-s", '-f', "foo -bar", p)
+        passed, skipped, failed = reprec.listoutcomes()
+        assert len(passed) == 1
+        assert passed[0].nodeid.split('::')[1] == 'test_bar'
+        
+    def test_check_collect_foo_but_not_bar_as_or(self, testdir):
+        p = testdir.makepyfile("""
+            import pytest
+
+            @pytest.mark.bar
+            def test_foo():
+                pass
+
+            @pytest.mark.foo
+            def test_bar():
+                pass
+
+            @pytest.mark.foo
+            @pytest.mark.bar
+            def test_baz():
+                pass
+        """)
+        reprec = testdir.inline_run("-s", '-f', "foo", "-f", "-bar", p)
+        passed, skipped, failed = reprec.listoutcomes()
+        assert len(passed) == 1
+        assert passed[0].nodeid.split('::')[1] == 'test_bar'
